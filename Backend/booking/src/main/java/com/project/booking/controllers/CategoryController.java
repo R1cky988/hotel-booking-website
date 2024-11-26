08c36161d7e7dfdb7c9a09b98c4c7138b0240a36
Huy Hà <Huy.HN225332@sis.hust.edu.vn>
2024-11-26 09:36:36 +0700

    Backend

---
+++ Backend/booking/src/main/java/com/project/booking/controllers/CategoryController.java
@@ -0,0 +1,19 @@
+package com.project.booking.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+    @PostMapping("")
+    public ResponseEntity<?> createCategory(){
+        return ResponseEntity.ok("create successfully");
+    }
+
+}
