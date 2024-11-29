dd9d17e6bc631ee3e56d2ce270c6d7865ab90c81
Huy Hà <Huy.HN225332@sis.hust.edu.vn>
2024-11-29 17:05:15 +0700

    add user

---
+++ Backend/booking/src/main/java/com/project/booking/models/Users.java
@@ -0,0 +1,31 @@
+package com.project.booking.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class Users extends Time{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "FirstName")
+    private String firstName;
+
+    @Column(name = "LastName")
+    private String lastName;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Password")
+    private String password;
+
+}
