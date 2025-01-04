77fda4b2dff7a0e30aef5554276e229ed7794f3d
Quoc Nguyen <quocnk2o4@gmail.com>
2025-01-05 00:28:46 +0700

    Delete Backend directory

--- Backend/booking/src/main/java/com/project/booking/exceptions/InvalidParamException.java
+++
@@ -1,7 +0,0 @@
-package com.project.booking.exceptions;
-
-public class InvalidParamException extends RuntimeException {
-    public InvalidParamException(String message) {
-        super(message);
-    }
-}
--- Backend/booking/src/main/java/com/project/booking/repositories/RoomImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.booking.repositories;
-
-import com.project.booking.models.RoomImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomImageRepository extends JpaRepository<RoomImage, Long> {
-}
