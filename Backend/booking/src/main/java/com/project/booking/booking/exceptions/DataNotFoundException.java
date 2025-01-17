0b2742f8d8dc106e58e5e4b3f1b5dce46bd141a3
Quoc Nguyen <quocnk2o4@gmail.com>
2025-01-17 15:07:37 +0700

    Delete Backend/booking/src/main/java/com/project/booking/booking directory

--- Backend/booking/src/main/java/com/project/booking/booking/exceptions/DataNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.project.booking.exceptions;
-
-public class DataNotFoundException extends RuntimeException {
-    public DataNotFoundException(String message) {
-        super(message);
-    }
-}
--- Backend/booking/src/main/java/com/project/booking/booking/exceptions/SamePasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.project.booking.exceptions;
-
-public class SamePasswordException extends RuntimeException {
-    public SamePasswordException(String message) {
-        super(message);
-    }
-}
