51617b4e878fda515378625652747f961a661936
Quoc Nguyen <quocnk2o4@gmail.com>
2025-01-17 14:58:06 +0700

    Delete java/com/project/booking directory

--- java/com/project/booking/repositories/HotelFacilityDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.booking.repositories;
-
-import com.project.booking.models.HotelFacilityDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelFacilityDetailRepository extends JpaRepository<HotelFacilityDetail, Long> {
-}
--- java/com/project/booking/repositories/RoomFacilityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.booking.repositories;
-
-import com.project.booking.models.RoomFacilities;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomFacilityRepository extends JpaRepository<RoomFacilities, Long> {
-}
